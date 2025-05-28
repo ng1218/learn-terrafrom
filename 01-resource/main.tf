9aea9a3b64ffb2fdccc918698b00cf4fe50f62bc
i340771 <naresh.ganta@sap.com>
2025-05-28 22:11:39 +0530

    test

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -1,6 +1,7 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"
 }
 
 resource "azurerm_virtual_machine" "frontend-test" {
