0a4509d9d0f7047a9da8827487c6928f864b3921
i340771 <naresh.ganta@sap.com>
2025-05-28 22:19:25 +0530

    test

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_machine" "frontend-test" {
   location              = "UK West"
   resource_group_name   = "myfirstvm_group"
   network_interface_ids = ["/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Network/networkInterfaces/terraform-test"]
-  vm_size               = "Standard_D2ls_v5"
+  vm_size               = "Standard_B2s"
 
   delete_os_disk_on_termination = true
 
