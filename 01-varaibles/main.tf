af1636672f62c17760b0d6b79e0f7bdd91042c3d
i340771 <naresh.ganta@sap.com>
2025-05-30 19:09:54 +0530

    test

--- 01-varaibles/main.tf
+++ 01-varaibles/main.tf
@@ -35,3 +35,36 @@ variable "datatypes1" {
 output "datatypes1" {
   value = var.datatypes1["z"]
 }
+
+
+## Variable from command line
+
+variable "cli" {
+  
+}
+
+output "command-line" {
+  value = var.cli
+}
+
+##Variable from terraform.tfvars
+##terraform apply -auto-approve -var.file=env.tfvar
+variable "terraformtfvards" {}
+
+output "Test Terraform Vars" {
+  value = var.terraformtfvards
+}
+
+## Varaibles from file
+variable "terraformvarfromfile" {}
+
+output "Test Terraform Vars" {
+  value = var.terraformvarfromfile
+}
+
+##{file-name}-auto.tfvars
+variable "autoapprovevars" {}
+
+output "Test Terraform Vars" {
+  value = var.autoapprovevars
+}
---
+++ 01-varaibles/server.auto.tfvars
@@ -0,0 +1,1 @@
+server-count=100
