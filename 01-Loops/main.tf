e07810f3e374c191df651f61171ef328db5a3ff1
i340771 <naresh.ganta@sap.com>
2025-06-01 13:58:06 +0530

    test

--- 01-Loops/main.tf
+++ 01-Loops/main.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "looptest" {
-  count = 2
+  count = 1
 }
 
 output "looptest" {
@@ -14,36 +14,41 @@ variable "nodes" {
     ]
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"
+output "nodes" {
+    value = null_resource.looptest[*].var.nodes[count.index]
+  
 }
 
-resource "azurerm_virtual_machine" "test" {
-  count                 =  length(var.nodes)
-  name                  = "${var.nodes[count.index]}-test"
-  location              = "UK West"
-  resource_group_name   = "myfirstvm_group"
-  network_interface_ids = ["/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Network/networkInterfaces/terraform-test"]
-  vm_size               = "Standard_B2s"
+# provider "azurerm" {
+#   features {}
+#   subscription_id = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"
+# }
 
-  delete_os_disk_on_termination = true
+# resource "azurerm_virtual_machine" "test" {
+#   count                 =  length(var.nodes)
+#   name                  = "${var.nodes[count.index]}-test"
+#   location              = "UK West"
+#   resource_group_name   = "myfirstvm_group"
+#   network_interface_ids = ["/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Network/networkInterfaces/terraform-test"]
+#   vm_size               = "Standard_B2s"
 
-  storage_image_reference {
-   id = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
-  }
-  storage_os_disk {
-    name              = "${var.nodes[count.index]}-test-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "${var.nodes[count.index]}-test"
-    admin_username = "azuser"
-    admin_password = "devops@123456"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
+#   delete_os_disk_on_termination = true
+
+#   storage_image_reference {
+#    id = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
+#   }
+#   storage_os_disk {
+#     name              = "${var.nodes[count.index]}-test-disk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "${var.nodes[count.index]}-test"
+#     admin_username = "azuser"
+#     admin_password = "devops@123456"
+#   }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+# }
