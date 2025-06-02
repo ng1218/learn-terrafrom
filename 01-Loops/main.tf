6a5240a522599ce7d7eb8af644af286bc1a6b940
i340771 <naresh.ganta@sap.com>
2025-06-02 12:14:17 +0530

    test

--- 01-Loops/main.tf
+++ 01-Loops/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_machine" "test" {
   name                  = "${var.nodes[count.index]}-vm"
   location              = "UK West"
   resource_group_name   = "myfirstvm_group"
-  network_interface_ids = ["/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Network/networkInterfaces/terraform-test"]
+  network_interface_ids = [azurerm_network_interface.privateip[count.index].id]
   vm_size               = "Standard_B2s"
 
   delete_os_disk_on_termination = true
