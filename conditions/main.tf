1afb254bcb52a026dd7cb4896d52ee016dea5835
i340771 <naresh.ganta@sap.com>
2025-06-03 10:28:15 +0530

    test

--- conditions/main.tf
+++ conditions/main.tf
@@ -28,6 +28,7 @@ resource "azurerm_network_interface" "privateip" {
 }
 
 resource "azurerm_virtual_machine" "test" {
+  for_each              = var.nodes
   count                 =  length(var.nodes)
   name                  = "${each.key}.vm"
   location              = "UK West"
