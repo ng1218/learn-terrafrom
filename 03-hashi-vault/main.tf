dce5efeea1ba17d549c120bc01b1586558a315f7
i340771 <naresh.ganta@sap.com>
2025-06-11 20:56:20 +0530

    test

--- 03-hashi-vault/main.tf
+++ 03-hashi-vault/main.tf
@@ -1,13 +1,16 @@
 provider "vault" {
  address = "https://vault.nareshdevops1218.online:8200"
+ token   = var.token
 }
+
+variable "token" {}
 
 data "vault_generic_secret" "secret" {
   path = "secret/demo/ssh"
 }
 
 resource "local_file" "foo" {
-  content  = data.vault_generic_secret.secret.data_json
+  content  = data.vault_generic_secret.secret.data
   filename = "tmp/vault-secrets"
 }
 
